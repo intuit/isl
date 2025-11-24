14fa07c3229c095992df8a58245b4f2343ad2645
Corneliu Tusnea <corneliu_tusnea at intuit.com / corneliu at acorns.com.au>
2025-11-25 08:05:43 +1100

    updated workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -121,9 +121,10 @@ subprojects {
         apply(plugin = "signing")
         extensions.configure<org.gradle.plugins.signing.SigningExtension> {
             if (System.getenv("SIGNING_KEY") != null) {
+                val signingKeyId = System.getenv("SIGNING_KEY_ID")
                 val signingKey = System.getenv("SIGNING_KEY")
-                val signingPassword = System.getenv("SIGNING_PASSWORD")
-                useInMemoryPgpKeys(signingKey, signingPassword)
+                val signingPassword = System.getenv("SIGNING_PASSWORD") ?: ""
+                useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
             }
             sign(extensions.getByType<PublishingExtension>().publications["maven"])
         }
